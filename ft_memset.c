ea84acfa3273e3057cc32222b7e81615fbe0e1b3
Ananda Gurgel <gurgel.ananda@gmail.com>
2021-09-17 23:02:35 -0300

    norminette ok

--- ft_memset.c
+++ ft_memset.c
@@ -6,18 +6,18 @@
 /*   By: agurgel <agurgel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/28 22:16:02 by agurgel           #+#    #+#             */
-/*   Updated: 2021/09/14 16:09:42 by agurgel          ###   ########.fr       */
+/*   Updated: 2021/09/17 21:28:13 by agurgel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memset(void *src, int c, size_t n)
+void	*ft_memset(void *src, int c, size_t len)
 {
 	unsigned char	*s;
 
 	s = src;
-	while (n-- > 0)
+	while (len -- > 0)
 		*s++ = c;
 	return (src);
 }
